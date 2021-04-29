b1dc18298eaba9ece1ac411b301d1c52f2e63d12
Scors4 <Scors4@users.noreply.github.com>
2021-04-29 15:56:37 -0700

    Portals *should* work
    Portal doorways should work
    Haven't tested just yet.
    Teleport and continuous movement work.
    Continuous turn works.
    OpenXR works.
    -- Button needs written
    -- Other scenes need to be made
    -- Scene manager needs to be written.

--- Assets/Scripts/Doorway.cs
+++ Assets/Scripts/Doorway.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.OpenXR;
 
 public class Doorway : MonoBehaviour
 {
     public Doorway linkedDoor;
     public GameObject doorway;
+    public Camera doorCamera;
 
-    private Camera doorCamera;
-    private RenderTexture doorRT;
-    private Material doorMat;
-    private MeshRenderer doorwayMR;
+    public RenderTexture doorRT;
+    public Material doorMat;
+    public MeshRenderer doorwayMR;
 
-    private void Awake()
+    private bool ignoreTransport = false;
+
+    private void Start()
     {
         if (linkedDoor == null)
             enabled = false;
@@ -26,35 +29,42 @@ public class Doorway : MonoBehaviour
         if (linkedDoor.linkedDoor == null)
         {
             linkedDoor.linkedDoor = this;
-            linkedDoor.SetupRenderTexture(this);
+            //linkedDoor.SetupRenderTexture(this);
         }
 
-        doorCamera = this.GetComponentInChildren<Camera>();
-
-        SetupRenderTexture(linkedDoor);
+        //SetupRenderTexture(linkedDoor);
     }
 
-    // Start is called before the first frame update
-    void Start()
+    /*void FixedUpdate()
     {
-        
+        linkedDoor.SetCameraOffset(transform.InverseTransformPoint(Camera.main.transform.position),
+                transform.InverseTransformDirection(Camera.main.transform.forward));
+    }*/
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(!ignoreTransport)
+        {
+            linkedDoor.ReceiveTransport();
+            other.transform.root.position = linkedDoor.transform.TransformPoint(transform.InverseTransformPoint(other.transform.root.position));
+            other.transform.root.rotation = Quaternion.LookRotation(linkedDoor.transform.TransformDirection(transform.InverseTransformPoint(other.transform.root.forward)));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnTriggerExit(Collider other)
     {
-        
+        ignoreTransport = false;
     }
 
     void SetupRenderTexture(Doorway linkedDoor)
     {
         doorwayMR = doorway.GetComponent<MeshRenderer>();
-        doorMat = new Material(Shader.Find("ScreenCutoutShader"));
+        doorMat = new Material(Shader.Find("Unlit/ScreenCutoutShader"));
         doorRT = new RenderTexture(512, 512, 32, RenderTextureFormat.ARGB32);
-        doorMat.SetTexture(0, doorRT);
+        doorMat.SetTexture("_MainTex", doorRT);
         Camera linkedCamera = linkedDoor.GetDoorCamera();
         linkedCamera.targetTexture = doorRT;
-        doorwayMR.material = doorMat;
+        doorwayMR.sharedMaterial = doorMat;
     }
 
     public Camera GetDoorCamera()
@@ -64,16 +74,18 @@ public class Doorway : MonoBehaviour
 
     public void SetCameraOffset(Vector3 offset, Vector3 direction)
     {
+        offset.x = -offset.x;
+        offset.z = -offset.z;
+
+        direction.x = -direction.x;
+        direction.z = -direction.z;
+
         doorCamera.transform.localPosition = offset;
-        
+        doorCamera.transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
     }
 
-    private void OnWillRenderObject()
+    public void ReceiveTransport()
     {
-        if(Camera.current == Camera.main)
-        {
-            linkedDoor.SetCameraOffset(transform.InverseTransformPoint(Camera.main.transform.position),
-                transform.InverseTransformDirection(Camera.main.transform.forward));
-        }
+        ignoreTransport = true;
     }
 }
